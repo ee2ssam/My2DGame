814c715371e0f504670ce329419a7a12a050f016
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-11-10 14:58:27 +0900

    1. 플레이어 공격 구현 2. 적 리소스 추가

---
+++ Assets/My2DGame/Scripts/EnemyController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace My2DGame
+{
+    /// <summary>
+    /// Enemy를 관리하는 클래스
+    /// </summary>
+    [RequireComponent (typeof(Rigidbody2D), typeof(TouchingDirections))]
+    public class EnemyController : MonoBehaviour
+    {
+
+    }
+}
